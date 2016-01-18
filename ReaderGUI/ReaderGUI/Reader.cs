1d4208b2e36256086b2a8f31f75900e07a54e29f
Marin Young <Marin Young>
2016-01-19 01:06:54 +0800

    add GUI project

---
+++ ReaderGUI/ReaderGUI/Reader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using SimpleLLRPSample;
+
+// ReSharper disable once CheckNamespace
+namespace SimpleLLRPSample
+{
+    public class Reader
+    {
+        public class ReaderParameters
+        {
+            public double TransmitPower { get; set; }
+            public ushort ModeIndex { get; set; }
+            public ushort TagPopulation { get; set; }
+            public ushort TagTransitTime { get; set; }
+            public bool[] AntennaId { get; set; }
+            public ushort ReaderSensitivity { get; set; }
+            public ushort ChannelIndex { get; set; }
+            public ushort HopTableIndex { get; set; }
+            public ushort PeriodicTriggerValue { get; set; }
+
+
+            public ReaderParameters()
+            {
+                TransmitPower = 32.5; // dbm
+                ModeIndex = 2;
+                TagPopulation = 1;
+                TagTransitTime = 0;
+                AntennaId = new bool[] { true, false, false, false }; // Support Multiple Antennas. R420, R220
+                ReaderSensitivity = 1;
+                ChannelIndex = 1;
+                HopTableIndex = 1;
+                PeriodicTriggerValue = 0;
+            } // end ReaderParameters constructor
+
+        } // end ReaderParameters
+    } // end Reader
+} // end namespace
---
+++ ReaderGUI/ReaderGUI/TagInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace ReaderGUI
+{
+    public class TagInfo
+    {
+        public string Epc { get; set; }
+        public string TimeStamp { get; set; }
+        public ushort Antenna { get; set; }
+        public float TxPower { get; set; }
+        public ushort ChannelIndex { get; set; }
+        public double Frequency { get; set; }
+        public int Rssi { get; set; }
+        public double PhaseInRadian { get; set; }
+        public double PhaseInDegree { get; set; }
+        public int DopplerShift { get; set; }
+        public double Velocity { get; set; }
+
+
+  
+    }
+}
